698c0b2d74b64f4cbf9c8f85ad7d2521f46390e2
miro <miroslav@luno.com>
2020-09-06 11:37:22 +0200

    example list view and cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,25 @@
 import 'package:flutter/material.dart';
+import 'cart.dart';
+import 'dish_object.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Place order'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,68 +27,209 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Dish> _dishes = List<Dish>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<Dish> _cartList = List<Dish>();
+
+  @override
+  void initState() {
+    super.initState();
+    _populateDishes();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0, top: 8.0),
+            child: GestureDetector(
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: <Widget>[
+                  Icon(
+                    Icons.shopping_cart,
+                    size: 36.0,
+                  ),
+                  if (_cartList.length > 0)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: CircleAvatar(
+                        radius: 8.0,
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        child: Text(
+                          _cartList.length.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+              onTap: () {
+                if (_cartList.isNotEmpty)
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => Cart(_cartList),
+                    ),
+                  );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildGridView(),
     );
+  }
+
+  ListView _buildListView() {
+    return ListView.builder(
+      itemCount: _dishes.length,
+      itemBuilder: (context, index) {
+        var item = _dishes[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8.0,
+            vertical: 2.0,
+          ),
+          child: Card(
+            elevation: 4.0,
+            child: ListTile(
+              leading: Icon(
+                item.icon,
+                color: item.color,
+              ),
+              title: Text(item.name),
+              trailing: GestureDetector(
+                child: (!_cartList.contains(item))
+                    ? Icon(
+                  Icons.add_circle,
+                  color: Colors.green,
+                )
+                    : Icon(
+                  Icons.remove_circle,
+                  color: Colors.red,
+                ),
+                onTap: () {
+                  setState(() {
+                    if (!_cartList.contains(item))
+                      _cartList.add(item);
+                    else
+                      _cartList.remove(item);
+                  });
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  GridView _buildGridView() {
+    return GridView.builder(
+        padding: const EdgeInsets.all(4.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: _dishes.length,
+        itemBuilder: (context, index) {
+          var item = _dishes[index];
+          return Card(
+              elevation: 4.0,
+              child: Stack(
+                fit: StackFit.loose,
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        item.icon,
+                        color: (_cartList.contains(item))
+                            ? Colors.grey
+                            : item.color,
+                        size: 100.0,
+                      ),
+                      Text(
+                        item.name,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.subhead,
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: 8.0,
+                      bottom: 8.0,
+                    ),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: GestureDetector(
+                        child: (!_cartList.contains(item))
+                            ? Icon(
+                          Icons.add_circle,
+                          color: Colors.green,
+                        )
+                            : Icon(
+                          Icons.remove_circle,
+                          color: Colors.red,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            if (!_cartList.contains(item))
+                              _cartList.add(item);
+                            else
+                              _cartList.remove(item);
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ));
+        });
+  }
+
+  void _populateDishes() {
+    var list = <Dish>[
+      Dish(
+        name: 'Chicken Zinger',
+        icon: Icons.fastfood,
+        color: Colors.amber,
+      ),
+      Dish(
+        name: 'Chicken Zinger without chicken',
+        icon: Icons.print,
+        color: Colors.deepOrange,
+      ),
+      Dish(
+        name: 'Rice',
+        icon: Icons.child_care,
+        color: Colors.brown,
+      ),
+      Dish(
+        name: 'Beef burger without beef',
+        icon: Icons.whatshot,
+        color: Colors.green,
+      ),
+      Dish(
+        name: 'Laptop without OS',
+        icon: Icons.laptop,
+        color: Colors.purple,
+      ),
+      Dish(
+        name: 'Mac wihout macOS',
+        icon: Icons.laptop_mac,
+        color: Colors.blueGrey,
+      ),
+    ];
+
+    setState(() {
+      _dishes = list;
+    });
   }
 }
